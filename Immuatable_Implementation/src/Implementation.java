5f017d4255fb41958c9d95a2071c61bb266dc6f2
siddhi saxena <siddhi.saxena@pearson.com>
2020-04-04 16:34:30 +0530

    Implement_Immutable

---
+++ Immuatable_Implementation/src/Implementation.java
@@ -0,0 +1,22 @@
+
+public final class Implementation {
+	
+	final String name;
+	final Integer id;
+	
+	public String getName() {
+		return name;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public Implementation(String name,Integer id)
+	{
+		this.name = name;
+		this.id = id;
+	}
+	
+
+}
---
+++ Immuatable_Implementation/src/MainImplementation.java
@@ -0,0 +1,11 @@
+
+public class MainImplementation {
+	
+	public static void main(String args[])
+	{
+		Implementation implement = new Implementation("Siddhi", 1214310164);
+				System.out.println(implement.getName());
+				System.out.println(implement.getId());
+	}
+
+}
