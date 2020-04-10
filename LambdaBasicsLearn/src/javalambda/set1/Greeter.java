352fcad69d2d3afaeaf13f306efb1bdb24b6c7fc
siddhi saxena <siddhi.saxena@pearson.com>
2020-04-10 18:34:38 +0530

    Lambda

---
+++ LambdaBasicsLearn/src/javalambda/set1/Greeter.java
@@ -0,0 +1,14 @@
+package javalambda.set1;
+
+public class Greeter {
+public void greet()
+{
+	System.out.println("Hello World");
+}
+
+public static void main(String args[])
+{
+	Greeter greeter = new Greeter();
+	greeter.greet();
+}
+}
